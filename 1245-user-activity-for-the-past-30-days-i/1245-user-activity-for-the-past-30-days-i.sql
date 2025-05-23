aa36c7779f77652385054b91cdb2cedf840742bd
GAURAV LODHI <lodhigaurav999@gmail.com>
2025-05-23 16:15:57 +0530

    Time: 464 ms (89.1%), Space: 0B (100%) - LeetHub

---
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,6 @@
+select activity_date day , count(distinct user_id) active_users 
+from Activity
+where activity_date between date_sub('2019-07-27', interval 29 day) and '2019-07-27'
+group by activity_date
+
+
